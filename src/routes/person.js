3a846901f6faafd127b1bf0ecd5007a65e2b8586
Adair Emanuel González Vega <51676730+adairvega@users.noreply.github.com>
2021-06-03 06:33:49 -0500

    Service completed for person

--- src/routes/person.js
+++ src/routes/person.js
@@ -5,9 +5,77 @@ const mysqlConnection = require('../database');
 
 
 router.get('/', (req, res) => {
-    mysqlConnection.query('SELECT * FROM people', (err, rows, fields) => {
+    mysqlConnection.query('SELECT * FROM person', (err, rows, fields) => {
         if (!err) {
             res.json(rows);
+        }else{
+            console.log(err);
+        }
+    });
+});
+
+router.get('/:idPerson', (req, res) => {
+    const {idPerson} = req.params;
+    mysqlConnection.query('SELECT * FROM person WHERE idPerson = ?',[idPerson], (err, rows, fields) => {
+        if (!err) {
+            res.json(rows);
+        }else{
+            console.log(err);
+        }
+    });
+});
+
+router.post('/addperson', (req, res) => {
+    const { curp, rfc, inekey, name, lastname, email, registrationDate, idAddress } = req.body;
+    const query = `
+    INSERT INTO person (CURP, RFC, INEKey, name, lastname, email, registrationDate, idAddress) VALUES (?,?,?,?,?,?,?,?);
+    `;
+    mysqlConnection.query(query, [curp, rfc, inekey, name, lastname, email, registrationDate, idAddress], (err, rows, fields) => {
+        if (!err) {
+            res.json({status: 'Persona agregada correctamente'});
+            console.log(rows);
+        }else{
+            console.log(err);
+        }
+    });
+});
+router.post('/addperson', (req, res) => {
+    const { curp, rfc, inekey, name, lastname, email, registrationDate, idAddress } = req.body;
+    const query = `
+    INSERT INTO person (CURP, RFC, INEKey, name, lastname, email, registrationDate, idAddress) VALUES (?,?,?,?,?,?,?,?);
+    `;
+    mysqlConnection.query(query, [curp, rfc, inekey, name, lastname, email, registrationDate, idAddress], (err, rows, fields) => {
+        if (!err) {
+            res.json({status: 'Persona agregada correctamente'});
+            console.log(rows);
+        }else{
+            console.log(err);
+        }
+    });
+});
+router.put('/updateperson/:idPerson', (req, res) => {
+    const { curp, rfc, inekey, name, lastname, email, registrationDate, idAddress } = req.body;
+    const { idPerson } = req.params;
+    const query = `
+    UPDATE person SET CURP = ?, RFC = ?, INEKey = ?, name = ?, lastname = ?, email = ?, 
+    registrationDate = ?, idAddress = ? WHERE (idPerson = ?);
+    `;
+    mysqlConnection.query(query, [curp, rfc, inekey, name, lastname, email, registrationDate, idAddress, idPerson], (err, rows, fields) => {
+        if (!err) {
+            res.json({status: 'Persona actualizada correctamente'});
+            console.log(rows);
+        }else{
+            console.log(err);
+        }
+    });
+});
+
+router.delete('/deleteperson/:idPerson', (req, res) => {
+    const { idPerson } = req.params;
+    mysqlConnection.query("DELETE FROM person WHERE idPerson = ?",[idPerson],(err,rows,fields)=>{
+        if (!err) {
+            res.json({status: 'Persona eliminada correctamente'});
+            console.log(rows);
         }else{
             console.log(err);
         }
